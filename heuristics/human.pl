09ef10230e1fcf71016f6464ef99ff7b80678222
Josselin MARNAT <josselin.marnat@gmail.com>
2016-10-25 14:03:38 +0200

    Add files via upload

---
+++ heuristics/human.pl
@@ -0,0 +1,24 @@
+askPiece_human(inline,Board,PieceID) :-
+	printAvailablePieces(Board),
+	write("Enter a piece ID : "),
+	read(PieceID),
+	isAvailable(Board,PieceID).
+	%% debugHere.
+
+askPiece_human(inline,Board,PieceID) :-
+	write("This piece is not available! Try again!"),nl,
+	askPiece_human(inline,Board,PieceID).
+
+
+
+
+readPosition_human(inline,Board,_,Row,Col) :-
+	write("[row,col]? "),
+	read([Row,Col]), 
+	Row > 0, Row < 5,
+	Col > 0, Col < 5,
+	isEmpty(Board,Row,Col).
+
+readPosition_human(inline,Board,PieceID,Row,Col) :-
+	write("Nope! Try again..."),nl,
+	readPosition_human(inline,Board,PieceID,Row,Col).
---
+++ heuristics/random.pl
@@ -0,0 +1,22 @@
+askPiece_random(inline,Board,PieceID) :-
+	getAvailablePieces(Board,ListOfAvailablePieces),
+	random_member(PieceID,ListOfAvailablePieces),
+	write("I chose the piece n°"),
+	write(PieceID),
+	write(" : "),
+	printPiece(PieceID),
+	nl.
+
+
+
+readPosition_random(inline,Board,_,Row,Col) :-
+	random_member(Row,[1,2,3,4]),
+	random_member(Col,[1,2,3,4]),
+	isEmpty(Board,Row,Col).
+
+readPosition_random(inline,Board,_,Row,Col) :-
+	random_member(Row,[1,2,3,4]),
+	random_member(Col,[1,2,3,4]),
+	isFull(Board,Row,Col),
+	readPosition_random(inline,Board,_,Row,Col).
+
