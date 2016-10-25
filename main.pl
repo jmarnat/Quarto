7184eb00603ddbe27cdd354452ecec790a5358ab
Josselin MARNAT <josselin.marnat@gmail.com>
2016-10-25 10:51:43 +0200

    new consult method

--- main.pl
+++ main.pl
@@ -1,4 +1,13 @@
-:- [quarto].
-:- [inline_interface].
+:- consult('quarto').
+:- consult('inline_interface').
+:- consult('include_ia/random').
+
 
 :- write("FILES LOADED !"),nl.
+
+/*
+- add your consults above
+- write this two functions :
+	askPiece(inline,<yourheuristics>,Board,PieceID)
+	readPosition(inline,<yourheuristics>,Board,PieceID,Row,Column)
+*/
