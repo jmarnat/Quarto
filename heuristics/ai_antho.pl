642648290653013fb5e9edf1b866ad0636089704
Drydge <antho.deveaux@gmail.com>
2016-10-26 15:54:39 +0200

    Giving piece Work

--- heuristics/ai_antho.pl
+++ heuristics/ai_antho.pl
@@ -7,23 +7,19 @@ invertAttribute(square,round).
 invertAttribute(hole,flat).
 invertAttribute(flat,hole).
 try(N):-
-	countPieces(N,[[0,2,3,4],[5,0,7,0],[0,0,0,12],[13,0,15,0]])
-.		
+	countPieces(N,[[0,2,3,4],[5,0,7,0],[0,0,0,12],[13,0,15,0]]).		
 
 
 countPieces(16,[]):-!.
 countPieces(Nu,[H|T]):-
 	countEmpty(NE,H),	
 	countPieces(N,T),
-	Nu is N-NE
-.
+	Nu is N-NE.
 countEmpty(NE,[0|T]):-
 	countEmpty(NET,T),
-	NE is NET+1
-.
+	NE is NET+1.
 countEmpty(NE,[_|T]):-
-	countEmpty(NE,T)
-.
+	countEmpty(NE,T).
 countEmpty(0,[]).
 
 invertPiece(PieceID,InvertID):-
@@ -32,8 +28,7 @@ invertPiece(PieceID,InvertID):-
 	invertAttribute(Height,IVHeight),
 	invertAttribute(Geometry,IVGeometry),
 	invertAttribute(Hole,IVHole),
-	piece(InvertID,[IVColor,IVHeight,IVGeometry,IVHole])
-.
+	piece(InvertID,[IVColor,IVHeight,IVGeometry,IVHole]).
 share3(PieceID,[P1,P2,P3,P4]):-
 	piece(PieceID,[Color,Height,Geometry,Hole]),
 	invertAttribute(Color,IVColor),
@@ -43,8 +38,7 @@ share3(PieceID,[P1,P2,P3,P4]):-
 	piece(P1,[Color,Height,Geometry,IVHole]),
 	piece(P2,[Color,Height,IVGeometry,Hole]),
 	piece(P3,[Color,IVHeight,Geometry,Hole]),
-	piece(P4,[IVColor,Height,Geometry,Hole])
-.
+	piece(P4,[IVColor,Height,Geometry,Hole]).
 share1(PieceID,[P1,P2,P3,P4]):-
 	piece(PieceID,[Color,Height,Geometry,Hole]),
 	invertAttribute(Color,IVColor),
@@ -54,8 +48,7 @@ share1(PieceID,[P1,P2,P3,P4]):-
 	piece(P1,[IVColor,IVHeight,IVGeometry,Hole]),
 	piece(P2,[IVColor,IVHeight,Geometry,IVHole]),
 	piece(P3,[IVColor,Height,IVGeometry,IVHole]),
-	piece(P4,[Color,IVHeight,IVGeometry,IVHole])
-.
+	piece(P4,[Color,IVHeight,IVGeometry,IVHole]).
 share2(PieceID,[P1,P2,P3,P4,P5,P6]):-
 	piece(PieceID,[Color,Height,Geometry,Hole]),
 	invertAttribute(Color,IVColor),
@@ -67,63 +60,80 @@ share2(PieceID,[P1,P2,P3,P4,P5,P6]):-
 	piece(P3,[Color,IVHeight,IVGeometry,Hole]),
 	piece(P4,[IVColor,Height,Geometry,IVHole]),
 	piece(P5,[IVColor,Height,IVGeometry,Hole]),
-	piece(P6,[IVColor,IVHeight,Geometry,Hole])
-.
+	piece(P6,[IVColor,IVHeight,Geometry,Hole]).
+
+getLoosesPieces(Board,ListOfPieces) :-
+	getLoosesPiecesBis(Board,ListOfPieces,1).
+
+getLoosesPiecesBis(_,[],17).
+getLoosesPiecesBis(Board,[PieceID|ListOfPieces],PieceID) :-
+	PieceID < 17,
+	NewPieceID is (PieceID + 1),
+	isLooses(Board,PieceID),
+	getLoosesPiecesBis(Board,ListOfPieces,NewPieceID).
+getLoosesPiecesBis(Board,ListOfPieces,PieceID) :-
+	PieceID < 17,
+	NewPieceID is (PieceID + 1),
+	getLoosesPiecesBis(Board,ListOfPieces,NewPieceID).
+isLooses(Board,PieceID):-
+	piece(TestPieceID,_),
+	putPieceOnBoard(TestPieceID,_,_,Board,NewBoard),
+	check_win(NewBoard,_,_,_),
+	PieceID is TestPieceID.
+
 
 askPiece_ai_antho(inline,Board,PieceID,MyPieceID):-
 	printAvailablePieces(Board),
 	countPieces(N,Board),
-	givePiece(N,Board,MyPieceID,PieceID),
-	write('I choose :'),write(PieceID),nl,nl,printPiece(PieceID),nl,nl
-.
+	getLoosesPieces(Board,LoosePieces),
+	getAvailablePieces(Board,AvailablePieces),
+
+	write(loosePieces),write(LoosePieces),nl,
+	givePiece(N,MyPieceID,PieceID,AvailablePieces,LoosePieces),
+	write('I choose :'),write(PieceID),nl,nl,printPiece(PieceID),nl,nl.
 
 readPosition_ai_antho(inline,Board,PieceID,Row,Col):-
-	choosePosition(Board,PieceID,Row,Col)
+	choosePosition(Board,PieceID,Row,Col).
+givePiece(0,_,PieceID,AvailablePieces,_):-
+	findPiece(0,_,PieceID,AvailablePieces)
 .
-givePiece(0,_,_,FirstPiece):-
-	random_member(FirstPiece,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]).
-givePiece(N,Board,MyPieceID,PieceID):-
-	N<5,
-	invertPiece(MyPieceID,PieceID),
-	isAvailable(Board,PieceID)
-	.
-	
+givePiece(N,MyPieceID,PieceID,AvailablePieces,LoosePieces):-
+	findPiece(N,MyPieceID,PieceID),
+	checkPiece(PieceID,AvailablePieces,LoosePieces).
+givePiece(N,MyPieceID,PieceID,AvailablePieces,_):-
+	findPiece(N,MyPieceID,PieceID,AvailablePieces).
 
-givePiece(N,Board,MyPieceID,PieceID):-
-	N<5,
+message(0).
+message(_):-write('Probably Loose').
+checkPiece(PieceID,AvailablePieces,LoosePieces):-
+	member(PieceID,AvailablePieces),
+	\+member(PieceID,LoosePieces).
+
+findPiece(_,_,RandomPiece,AvailablePieces):-
+	random_member(RandomPiece,AvailablePieces).
+findPiece(N,MyPieceID,PieceID):-
+	N>0,N<5,
+	invertPiece(MyPieceID,PieceID).
+findPiece(N,MyPieceID,PieceID):-
+	N>0,N<5,
 	share1(MyPieceID,PiecesList),
-	write(PiecesList),nl,
-	chooseAvailable(Board,PieceID,PiecesList)
-	.
-
-givePiece(_,Board,_,PieceID):-
+	member(PieceID,PiecesList),
+	write(PiecesList),nl.
+findPiece(N,MyPieceID,PieceID):-
+	N>4,
 	write(' Share 3 : '),nl,
-	share3(PieceID,PiecesList),
-	chooseAvailable(Board,PieceID,PiecesList)
-	.
-givePiece(_,Board,_,PieceID):-
+	share3(MyPieceID,PiecesList),
+	member(PieceID,PiecesList),
+	write(PiecesList),nl.
+findPiece(N,MyPieceID,PieceID):-
+	N>4,
 	write(' Share  2 : '),nl,
-	share2(PieceID,PiecesList),
-	chooseAvailable(Board,PieceID,PiecesList)
-	.
-givePiece(_,Board,_,PieceID):-
+	share2(MyPieceID,PiecesList),
+	member(PieceID,PiecesList),
+	write(PiecesList),nl.
+findPiece(N,MyPieceID,PieceID):-
+	N>4,
 	write(' Share 1 : '),nl,
-	share1(PieceID,PiecesList),
-	chooseAvailable(Board,PieceID,PiecesList)
-	.
-
-chooseAvailable(_,_,[]):-
-	fail,!.
-
-chooseAvailable(Board,ID,[ID|_]):-
-	read(_),
-	isAvailable(Board,ID).
-
-chooseAvailable(Board,PieceID,[_|T]):-
-	chooseAvailable(Board,PieceID,T).	
-
-%% goodChoose(Board,PieceID):-
-%% 	write('Choose : '), write(PieceID), nl,
-%% 	piece(PieceID,PieceAtt),
-%% 	check_win(Board,Attribute,_,_),
-%% 	\+member(Attribute,PieceAtt),!.
+	share1(MyPieceID,PiecesList),
+	member(PieceID,PiecesList),
+	write(PiecesList),nl.
