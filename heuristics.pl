f81471e5322b32bc4a9ffeefa2703d8bc7cecada
Drydge <antho.deveaux@gmail.com>
2017-01-08 17:58:18 +0100

    resolve running AI with not inline mode

--- heuristics.pl
+++ heuristics.pl
@@ -23,45 +23,43 @@ heuristics(josselin).
 heuristics(jeremie).
 %% heuristics().
 
-
-
 /* remapping all the askPiece and readPosition functions */
 /* because Prolog don't like separated clauses definitions */
 /* "Clauses of askPiece/5 are not together in the source-file" */
 
-askPiece(inline,human,Board,PieceID,_) :-
+askPiece(_,human,Board,PieceID,_) :-
 	askPiece_human(inline,Board,PieceID).
 
-askPiece(inline,random,Board,PieceID,_) :-
+askPiece(_,random,Board,PieceID,_) :-
 	askPiece_random(inline,Board,PieceID).
 
-askPiece(inline,anthony,Board,PieceID,LastPieceId) :-
+askPiece(_,anthony,Board,PieceID,LastPieceId) :-
 	askPiece_anthony(inline,Board,PieceID,LastPieceId).
 
-askPiece(inline,josselin,Board,PieceID,_LastPieceId) :-
+askPiece(_,josselin,Board,PieceID,_LastPieceId) :-
 	askPiece_josselin(inline,Board,PieceID).
 
-askPiece(inline,clement,Board,PieceID,LastPieceId) :-
+askPiece(_,clement,Board,PieceID,LastPieceId) :-
 	askPiece_clement(inline,Board,PieceID,LastPieceId).
 
-askPiece(inline,jeremie,Board,PieceID,_) :-
+askPiece(_,jeremie,Board,PieceID,_) :-
 	askPiece_jeremie(inline,Board,PieceID).
 
-readPosition(inline,human,Board,PieceID,Row,Col) :-
+readPosition(_,human,Board,PieceID,Row,Col) :-
 	write('Piece to play : '),printPiece(PieceID),nl,
 	readPosition_human(inline,Board,PieceID,Row,Col).
 
-readPosition(inline,random,Board,PieceID,Row,Col) :-
+readPosition(_,random,Board,PieceID,Row,Col) :-
 	readPosition_random(inline,Board,PieceID,Row,Col).
 
-readPosition(inline,anthony,Board,PieceID,Row,Col) :-
+readPosition(_,anthony,Board,PieceID,Row,Col) :-
 	readPosition_anthony(inline,Board,PieceID,Row,Col).
 
-readPosition(inline,josselin,Board,PieceID,Row,Col) :-
+readPosition(_,josselin,Board,PieceID,Row,Col) :-
 	readPosition_josselin(inline,Board,PieceID,Row,Col).
 
-readPosition(inline,clement,Board,PieceID,Row,Col) :-
+readPosition(_,clement,Board,PieceID,Row,Col) :-
 	readPosition_clement(inline,Board,PieceID,Row,Col).
 
-readPosition(inline,jeremie,Board,PieceID,Row,Col) :-
+readPosition(_,jeremie,Board,PieceID,Row,Col) :-
 	readPosition_jeremie(inline,Board,PieceID,Row,Col).
